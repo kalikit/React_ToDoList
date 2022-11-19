5dc020b33bc0e607229b227b4805fc41e061e34a
QuanDucThang <77271685+kalikit@users.noreply.github.com>
2022-11-20 02:38:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { ReactDOM } from 'react-dom/client';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import { data } from './data';
